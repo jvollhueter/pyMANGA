1b45f54ff4ad0f81783faed22980942762c24f19
jonas <jonas.vollhueter@tu-dresden.de>
2020-03-11 12:59:50 +0100

    Erstellung eines ersten Versuchs der Programmierung einer Testabfrage

--- Demo.py
+++ Demo.py
@@ -2,6 +2,7 @@ from ProjectLib import XMLtoProject
 from TimeLoopLib import TreeDynamicTimeStepping
 import unittest
 
+<<<<<<< HEAD
 class TestNeueKlasse(unittest.TestCase):	
     	def test_code_run_files_without_errors(self):
         	try:
@@ -12,3 +13,16 @@ class TestNeueKlasse(unittest.TestCase):
             		self.fail("Fehler")
 if __name__ == '__main__':
     unittest.main()
+=======
+
+class TestNeueKlasse(unittest.TestCase):
+    def code_run_files_without_errors(self): #zugriff auf alle Attribute in Klasse
+        try:
+            prj = XMLtoProject(xml_project_file="Demo3.xml")
+            time_stepper = TreeDynamicTimeStepping(prj)
+            prj.runProject(time_stepper)
+        except:
+            self.fail("Fehler")
+if __name__ == '__main__':
+    unittest.main()
+>>>>>>> Erstellung eines ersten Versuchs der Programmierung einer Testabfrage
